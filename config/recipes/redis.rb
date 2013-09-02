39f9d1f10c3655ba40983abed9d8d872374fb5df
Nilson <tenaz3.comp@gmail.com>
2013-09-02 19:28:54 -0300

    redis config

--- config/recipes/redis.rb
+++ config/recipes/redis.rb
@@ -1,11 +1,23 @@
 namespace :redis do
+  # Install redis from apt repository ppa
+  #desc "Install latest stable release of redis"
+  #task :install, :roles => :web do
+  #  run "#{sudo} add-apt-repository ppa:rwky/redis", :pty => true do |ch, stream, data|
+  #    press_enter(ch, stream, data)
+  #  end
+  #  run "#{sudo} apt-get -y update"
+  #  run "#{sudo} apt-get -y install redis-server"
+  #end
+  #after "deploy:install", "redis:install"
+
   desc "Install latest stable release of redis"
   task :install, :roles => :web do
-    run "#{sudo} add-apt-repository ppa:rwky/redis", :pty => true do |ch, stream, data|
-      press_enter(ch, stream, data)
-    end
-    run "#{sudo} apt-get -y update"
-    run "#{sudo} apt-get -y install redis-server"
+    [
+        "wget http://download.redis.io/redis-stable.tar.gz",
+        "tar xvzf redis-stable.tar.gz",
+        "cd redis-stable/ && make"
+    ].each {|cmd| run cmd}
+
   end
   after "deploy:install", "redis:install"
 
@@ -13,8 +25,11 @@ namespace :redis do
   task :setup, :roles => :app do
 
     # Copie redis-server and redis-cli executables
-    run "#{sudo} cp /usr/bin/redis-server /usr/local/bin"
-    run "#{sudo} cp /usr/bin/redis-cli /usr/local/bin"
+    run "#{sudo} cp /home/#{user}/redis-stable/src/redis-server /usr/local/bin"
+    run "#{sudo} cp /home/#{user}/redis-stable/src/redis-cli /usr/local/bin"
+
+    #run "#{sudo} cp /usr/bin/redis-server /usr/local/bin"
+    #run "#{sudo} cp /usr/bin/redis-cli /usr/local/bin"
 
     # Create a directory where to store your Redis config files and your data:
     run "#{sudo} mkdir -p /etc/redis"
@@ -25,7 +40,7 @@ namespace :redis do
 
     # Make a writable log file
     run "#{sudo} touch /var/log/redis_6379.log"
-    run "#{sudo} chown #{user}:redis_6379 /var/log/redis_6379.log"
+    run "#{sudo} chown #{user} /var/log/redis_6379.log"
     run "#{sudo} chmod u+w /var/log/redis_6379.log"
 
     # Make an config file
